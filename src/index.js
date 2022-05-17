abb5b1ad599548a10d7598d90cdfe21adc9da312
pabloq <pablo_q_@hotmail.com>
2022-05-16 21:23:31 -0300

    coRRECCION DE port

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ require('./database');
 require('./config/passport');
 
 //settings
-app.set('port', process.env.port || 3000);
+app.set('port', process.env.PORT || 3000);
 app.set('views',path.join(__dirname,'views'))
 app.engine('.hbs',exphbs.engine({
     defaultlLayout:'main',
